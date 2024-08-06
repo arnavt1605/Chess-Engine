7ba473b965a9ca3aa1cb098ec039016558a8da1a
Arnav <arnavtripathi1605@gmail.com>
2024-08-07 00:17:05 +0530

    Making slow progress

---
+++ Chess_engine.py
@@ -0,0 +1,23 @@
+# This class is responsible for storing the current state of a chess game. 
+# It will also be responsible for determining the valid moves for the current state.
+# A move log will also be made and maintained.
+
+'''
+Naming Scheme of the pieces: First letter represents the colour of the piece: w for white and b for black.
+Next letter represents the type of piece: R for rook, N for knight, B for bishop, Q for queen, K for king, p for pawns.
+-- represents an empty square
+'''
+
+class GameState():
+    def __init__(self):
+        self.board=[
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR" ],
+            ["bp","bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp","wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR" ]]
+        self.whiteToMove=True 
+        self.moveLog= []    # To keep track of the previous moves.
---
+++ Chess_main.py
@@ -0,0 +1,12 @@
+#This is the main driver file responsible for user input and display the current state of game
+
+import pygame as p
+from Chess import Chess_engine 
+
+width = height =512
+dimension = 8 
+sq_size= height//dimension
+max_fps=15   #for the animations later on
+
+
+
