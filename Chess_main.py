c0a94198def983587f54f5cfa38358e33854b5ba
Arnav <arnavtripathi1605@gmail.com>
2024-08-07 00:18:15 +0530

    yo

--- Chess_main.py
+++ Chess_main.py
@@ -1,12 +1,13 @@
 #This is the main driver file responsible for user input and display the current state of game
 
 import pygame as p
-from Chess import Chess_engine 
+from Chess import Chess_engine  #type: ignore
 
 width = height =512
 dimension = 8 
 sq_size= height//dimension
 max_fps=15   #for the animations later on
+images={}
 
 
 
