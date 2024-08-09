71f12d08905b4e29609efba5ac0c9e1f86114ac0
Arnav <arnavtripathi1605@gmail.com>
2024-08-09 23:45:44 +0530

    Added

--- Chess_main.py
+++ Chess_main.py
@@ -10,4 +10,33 @@ max_fps=15   #for the animations later on
 images={}
 
 
+def loadImages():
+    pieces= ['bR','bN','bB','bQ','bK','bp','wR','wN','wB','wQ','wK','wp']
+    for piece in pieces:
+        images[piece]=p.image.load("images/"+piece+".png")
 
+def main():
+    screen= p.display.set_mode((width,height))
+    clock=p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs= Chess_engine.GameState()
+    loadImages()
+    running=True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running=False
+        drawGameState(screen,gs)
+        clock.tick(max_fps)
+        p.display.flip()
+
+
+def drawGameState(screen,gs):
+    drawBoard(screen)   #draws the squarees on the board
+    drawPieces(screen,gs.board)   #draws the pieces on the squares
+
+
+def drawBoard(screen):
+    
+if __name__ == "__main__":
+    main()
